5e2142aa85c94e11dac52f8e66bb78e322edbf43
reinaldo luiz <reinaldolluiz@gmail.com>
2020-08-15 21:13:12 -0300

    TEST - Admin view car model

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -1,5 +1,8 @@
 class CarModelsController < ApplicationController
-    def index
-        @car_models = CarModel.all
-    end
+  def index
+      @car_models = CarModel.all
+  end
+  def show
+    @car_model = CarModel.find(params[:id])
+  end
 end
