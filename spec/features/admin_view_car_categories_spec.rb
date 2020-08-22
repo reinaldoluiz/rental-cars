e44966537fda9e3f2c469118c8675ab2baf2590d
reinaldo luiz <reinaldolluiz@gmail.com>
2020-08-22 17:54:10 -0300

    TEST - Adim view car models in car categories

--- spec/features/admin_view_car_categories_spec.rb
+++ spec/features/admin_view_car_categories_spec.rb
@@ -89,4 +89,26 @@ feature 'Admin view car categories' do
 
     expect(current_path).to eq car_categories_path
   end
+  scenario 'and view car models' do
+    top = CarCategory.create!(name: 'Top', daily_rate: 105.5, car_insurance: 58.5,
+                        third_party_insurance: 10.5)
+    CarModel.create!(name:'Ka', year:2019, manufacturer:'Ford', motorization:'1.0',
+    car_category: top, fuel_type:'Flex')
+    CarModel.create!(name:'Fiesta', year:2021, manufacturer:'Chevrolet', motorization:'1.0',
+    car_category: top, fuel_type:'Flex')
+
+    user = User.create!(name:'João Almeida', email:'joao@email.com', password:'12345678')
+
+    login_as(user, scope: :user)
+
+    visit root_path
+    click_on 'Categorias'
+    click_on top.name
+
+    expect(page).to have_content('Top')
+    expect(page).to have_link('Ka')
+    expect(page).to have_content('2019')
+    expect(page).to have_link('Fiesta')
+    expect(page).to have_content('2021')
+  end
 end
